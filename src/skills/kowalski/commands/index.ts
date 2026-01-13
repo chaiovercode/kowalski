699b18b64752cfa52d59958db9c190d25bc97f06
chaiovercode <chaiovercode@gmail.com>
2026-01-13 14:31:46 +0530

    Remove unused test, debug, and build files

--- src/skills/kowalski/commands/index.ts
+++
@@ -1,6 +0,0 @@
-// Kowalski Commands - Export all command handlers
-
-export { reconSweep, formatReconResult, scanDirectory } from "./recon";
-export { analyzeFile, loadDataFile, performAnalysis, formatAnalysisSummary } from "./analyze";
-export { compareFiles } from "./compare";
-export { getHelpText } from "./help";
